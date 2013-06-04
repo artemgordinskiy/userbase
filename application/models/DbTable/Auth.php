faeabfa4df527a6ee7e24f5587b9cce901ace1c0
Artem <artemjo@gmail.com>
2013-06-04 14:02:30 +0300

    Cleaned up the Auth model a bit

--- application/models/DbTable/Auth.php
+++ application/models/DbTable/Auth.php
@@ -1,7 +1,6 @@
 <?php
 
-class Application_Model_DbTable_Auth extends Zend_Db_Table_Abstract {
-    protected $_name = 'groups';
+class Application_Model_DbTable_Auth {
     protected $_login = null;
     protected $_password = null;
 
@@ -11,19 +10,16 @@ class Application_Model_DbTable_Auth extends Zend_Db_Table_Abstract {
     }
 
     public function authorize() {
-        $staticSalt = ADMIN_STATIC_PASS_SALT;
-        $adapter = new Zend_Auth_Adapter_DbTable(
-            null,
-            'admins',
-            'username',
-            'password',
-            "MD5(CONCAT('" . $staticSalt . "', ?, pass_salt))"
-            );
+        $staticSalt = mysql_real_escape_string(ADMIN_STATIC_PASS_SALT);
+        $adapter = new Zend_Auth_Adapter_DbTable();
+        $adapter->setTableName('admins')
+                ->setIdentityColumn('username')
+                ->setIdentity($this->_login)
+                ->setCredentialColumn('password')
+                ->setCredential($this->_password)
+                ->setCredentialTreatment('MD5(CONCAT("' . $staticSalt . '", ?, pass_salt))');
 
-        $adapter->setIdentity($this->_login);
-        $adapter->setCredential($this->_password);
-
-        $auth   = Zend_Auth::getInstance();
+        $auth = Zend_Auth::getInstance();
         $result = $auth->authenticate($adapter);
         $isValid = $result->isValid();
         return $isValid;
