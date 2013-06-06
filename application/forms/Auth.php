ece506d4e0bfdc0745c114d753c5eb4abd7da44d
Artem <artemjo@gmail.com>
2013-06-06 20:12:16 +0300

    Converted the Login form to the viewscript

--- application/forms/Auth.php
+++ application/forms/Auth.php
@@ -8,25 +8,39 @@ class Application_Form_Auth extends Zend_Form
         $this->setName('auth')
              ->setMethod('post')
              ->setAttribs(array('class'=>'form-horizontal login-form'));
+
         $username = new Zend_Form_Element_Text('username');
-        $username->setLabel('Логин')
-                 ->setRequired(true)
+        $username->setRequired(true)
                  ->addFilter('StripTags')
                  ->addFilter('StringTrim')
                  ->addValidator('NotEmpty')
-                 ->addValidator('StringLength', false, array(5, 50));
+                 ->addValidator('StringLength', false, array(5, 50))
+                 ->removeDecorator('htmlTag')
+                 ->removeDecorator('DtDdWrapper')
+                 ->removeDecorator('label');
 
         $password = new Zend_Form_Element_Password('password');
-        $password->setLabel('Пароль')
-                 ->setRequired(true)
+        $password->setRequired(true)
                  ->addFilter('StripTags')
                  ->addFilter('StringTrim')
                  ->addValidator('NotEmpty')
-                 ->addValidator('StringLength', false, array(5, 50));
+                 ->addValidator('StringLength', false, array(5, 50))
+                 ->removeDecorator('htmlTag')
+                 ->removeDecorator('DtDdWrapper')
+                 ->removeDecorator('label');
 
         $submit = new Zend_Form_Element_Submit('submit');
         $submit->setLabel('Войти')
-               ->setAttribs(array('class'=>'btn'));
+               ->setAttribs(array('class'=>'btn'))
+               ->removeDecorator('DtDdWrapper');
+
+        $this->setDecorators(array(
+            array('ViewScript', array(
+                'viewScript' => '_form_login.phtml'
+                )
+            )
+        ));
+
         $this->addElements(array($username, $password, $submit));
 
     }
